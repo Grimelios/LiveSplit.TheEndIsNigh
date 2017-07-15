9aafbecf72832b2f738a370f6a27fab716532555
Mark Hatfield <grimelios@gmail.com>
2017-07-15 02:07:18 -0500

    Adding a testing class for later.

---
+++ LiveSplit.TheEndIsNigh/EndIsNighTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiveSplit.TheEndIsNigh
+{
+	/// <summary>
+	/// Static class used to test the autosplitter using the console.
+	/// </summary>
+	public static class EndIsNighTester
+	{
+		/// <summary>
+		/// Main function for the testing program.
+		/// </summary>
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
