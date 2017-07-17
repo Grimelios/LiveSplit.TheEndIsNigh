4398ce181c5f34c63a015e73aca57318fa5521cf
Mark <grimelios@gmail.com>
2017-07-16 22:16:30 -0500

    Adding a title using the game's font.

--- LiveSplit.TheEndIsNigh/Controls/EndIsNighControl.Designer.cs
+++ LiveSplit.TheEndIsNigh/Controls/EndIsNighControl.Designer.cs
@@ -30,31 +30,43 @@
 		{
 			this.defaultCategoryControl1 = new LiveSplit.TheEndIsNigh.Controls.DefaultCategoryControl();
 			this.splitCollectionControl1 = new LiveSplit.TheEndIsNigh.Controls.SplitCollectionControl();
+			this.autosplitterLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// defaultCategoryControl1
 			// 
-			this.defaultCategoryControl1.Location = new System.Drawing.Point(4, 4);
+			this.defaultCategoryControl1.Location = new System.Drawing.Point(3, 39);
 			this.defaultCategoryControl1.Name = "defaultCategoryControl1";
 			this.defaultCategoryControl1.Size = new System.Drawing.Size(207, 47);
 			this.defaultCategoryControl1.TabIndex = 0;
 			// 
 			// splitCollectionControl1
 			// 
-			this.splitCollectionControl1.Location = new System.Drawing.Point(4, 58);
+			this.splitCollectionControl1.Location = new System.Drawing.Point(3, 93);
 			this.splitCollectionControl1.Name = "splitCollectionControl1";
 			this.splitCollectionControl1.Size = new System.Drawing.Size(370, 258);
 			this.splitCollectionControl1.TabIndex = 1;
+			// 
+			// autosplitterLabel
+			// 
+			this.autosplitterLabel.AutoSize = true;
+			this.autosplitterLabel.Location = new System.Drawing.Point(3, 4);
+			this.autosplitterLabel.Name = "autosplitterLabel";
+			this.autosplitterLabel.Size = new System.Drawing.Size(139, 13);
+			this.autosplitterLabel.TabIndex = 2;
+			this.autosplitterLabel.Text = "The End Is Nigh Autosplitter";
 			// 
 			// EndIsNighControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.autosplitterLabel);
 			this.Controls.Add(this.splitCollectionControl1);
 			this.Controls.Add(this.defaultCategoryControl1);
 			this.Name = "EndIsNighControl";
 			this.Size = new System.Drawing.Size(535, 408);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -62,5 +74,6 @@
 
 		private DefaultCategoryControl defaultCategoryControl1;
 		private SplitCollectionControl splitCollectionControl1;
+		private System.Windows.Forms.Label autosplitterLabel;
 	}
 }
--- LiveSplit.TheEndIsNigh/Controls/EndIsNighControl.cs
+++ LiveSplit.TheEndIsNigh/Controls/EndIsNighControl.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveSplit.TheEndIsNigh.Properties;
 
 namespace LiveSplit.TheEndIsNigh.Controls
 {
+	/// <summary>
+	/// Partial class for the main End Is Nigh control.
+	/// </summary>
 	public partial class EndIsNighControl : UserControl
 	{
+		private const int FontSize = 16;
+
+		// Taken from http://stackoverflow.com/questions/556147/how-to-quickly-and-easily-embed-fonts-in-winforms-app-in-c-sharp.
+		[DllImport("gdi32.dll")]
+		private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [In] ref uint pcFonts);
+
+		/// <summary>
+		/// Constructs the class.
+		/// </summary>
 		public EndIsNighControl()
 		{
 			InitializeComponent();
+			LoadFont();
+		}
+
+		/// <summary>
+		/// Loads the The End font and applies it to the autosplitter label.
+		/// </summary>
+		private void LoadFont()
+		{
+			byte[] fontData = Resources.TheEnd;
+
+			IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+			Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+			PrivateFontCollection fontCollection = new PrivateFontCollection();
+
+			uint dummy = 0;
+
+			fontCollection.AddMemoryFont(fontPtr, fontData.Length);
+			AddFontMemResourceEx(fontPtr, (uint)fontData.Length, IntPtr.Zero, ref dummy);
+
+			Marshal.FreeCoTaskMem(fontPtr);
+
+			autosplitterLabel.Font = new Font(fontCollection.Families[0], FontSize);
 		}
 	}
 }
